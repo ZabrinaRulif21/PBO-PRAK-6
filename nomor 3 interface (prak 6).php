3b5d7611d78c66b23a8385dc3cfe3b7975d0eb2f
ZabrinaRulif21 <100702739+ZabrinaRulif21@users.noreply.github.com>
2022-10-20 09:06:55 -0700

    Add files via upload

---
+++ nomor 3 interface (prak 6).php
@@ -0,0 +1,14 @@
+<!-- Zabrina Rulif_21091397056-->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
